674f1a3bc43bf0bac8fa63eff3d7554835498e95
Thomaz Alves <thomazhilario5@gmail.com>
2023-10-06 12:20:45 -0300

    Primeiro app em react

---
+++ myapp/src/App.js
@@ -0,0 +1,17 @@
+// adicionando estilos ao app
+import './app.css'
+
+
+// Criando um componente exportado.
+
+
+export default function App(){
+  return(
+    <main>
+      <h1>Seja bem vindo ao meu primeiro app em ReactJS: Hello World</h1>
+      <h2>@ThomazHilario</h2>
+    </main>
+  )
+}
+
+// Depois de criado e exportado basta importalo no index.js
