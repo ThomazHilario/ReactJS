048d4b381bac073fde6727cd075b6159b38869f4
Thomaz Alves <thomazhilario5@gmail.com>
2023-10-06 22:29:57 -0300

    Entendendo componente funcional

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,15 +1,24 @@
 // adicionando estilos ao app
 import './app.css'
 
+// Criando um componente funcional: que nao tem estado / ciclo de vida (funções javaScript)
+function ExibindoAlgo(props){
+  // Podemos ter props que são atributos que podem ser passados no componente.
 
-// Criando um componente exportado.
+  // usamos return para retornar algo do component
+  return(
 
-
+    // abrindo chaves {}, dentro podemos usar javascript, assim manipulando props como se fosse um objeto.
+    <h2>Seja bem vindo {props.nome}</h2>
+  )
+}
+// componente exportado.
 export default function App(){
   return(
     <main>
       <h1>Seja bem vindo ao meu primeiro app em ReactJS: Hello World</h1>
-      <h2>@ThomazHilario</h2>
+      <ExibindoAlgo nome="ThomazHilario"/>
+      <ExibindoAlgo nome="Matheus"/>
     </main>
   )
 }
