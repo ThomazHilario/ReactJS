f845ad1013b81bbecf83a7349bbf3383d45456b7
Thomaz Alves <thomazhilario5@gmail.com>
2023-10-18 10:31:51 -0300

    retiando componente de app.js

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,12 +1,10 @@
 // adicionando estilos ao app
 import './app.css'
-import Rotas from './Components - aulas/Aprendizado/14 - Rotas'
 // componente exportado.
 export default function App(){
   return(
     <main>
       <h2>Seja bem vindo ao meu app react de aprendizados</h2>
-      <Rotas/>
     </main>
   )
 }
