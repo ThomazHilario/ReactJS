b9a678d1ac7a56ba4a5a1af3c741ee25cd68d185
Thomaz Alves <thomazhilario5@gmail.com>
2023-11-02 12:29:12 -0300

    Cadastrando e pegando usuarios do banco de dados

--- firebase-app/src/App.js
+++ firebase-app/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import RoutesPage from './pages/routes'
-
+import 'react-toastify/dist/ReactToastify.css';
+import {ToastContainer} from 'react-toastify'
 export default function App() {
   return (
     <div className="App">
+      <ToastContainer autoClose={2000}/>
       <RoutesPage/>
     </div>
   );
--- firebase-app/src/pages/home/index.js
+++ firebase-app/src/pages/home/index.js
@@ -1,6 +1,39 @@
 import {useState} from 'react'
+import {toast} from 'react-toastify'
+import database from '../../database/firebaseConnection'
+import {collection, addDoc, getDocs} from 'firebase/firestore'
 import './home.css'
 export default function Home(){
+    const [inputName,setName] = useState('')
+    const [inputAge,setAge] = useState('')
+
+    // Adicionando usuarios ao banco de dados
+    async function addUser(e){
+        e.preventDefault()
+        try {
+            await addDoc(collection(database,'users'), {
+                Name:inputName,
+                Age:inputAge
+            })
+
+            toast.success("usuario Cadastrado com sucesso")
+        } catch (error) {
+            toast.error("Usuario nao cadastrado")
+        }
+    }
+
+    // Pegando todos os usuarios
+    async function getUsers(){
+
+        // Fazendo a requisicao para pegar todos os usuarios
+        const docSnap = await getDocs(collection(database,'users'))
+
+        // percorrendo array
+        docSnap.forEach(element => {
+            console.log(element.data())
+        });
+
+    }
     return(
         <div id='container'>
             <fieldset>
@@ -8,20 +41,20 @@ export default function Home(){
                 <form>
                         <div className='container_input'>
                             <label>Name:</label>
-                            <input type='text'/>
+                            <input type='text' value={inputName} onChange={(e) => setName(e.target.value)}/>
                         </div>
 
                         <div className='container_input'>
                             <label>Age:</label>
-                            <input type='number'/>
+                            <input type='number' value={inputAge} onChange={(e) => setAge(e.target.value)}/>
                         </div>
 
                         <div>
-                            <button>Cadastrar</button>
+                            <button onClick={addUser}>Cadastrar</button>
                         </div>
                 </form>
             </fieldset>
-            
+            <button onClick={getUsers}>Pegar usuarios</button>
         </div>
     )
 }
