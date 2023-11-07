72e78a4e1880e11d55a6581e30ee412d507eeccc
Thomaz Alves <thomazhilario5@gmail.com>
2023-11-07 10:29:54 -0300

    Criando Login com firebase

--- auth-users/src/pages/home/index.js
+++ auth-users/src/pages/home/index.js
@@ -1,44 +1,103 @@
 import './home.css'
 import {useState} from 'react'
 import {auth} from '../../service/firebaseconnection'
-import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
 
 export default function Home(){
     // states - formulario
     const [email,setEmail] = useState('')
     const [password,setPassword] = useState('')
 
+
+    // state - login
+    const [login, setLogin] = useState(false)
+    const [detail, setDetail] = useState({})
+
     // function cadastro
     async function newUser(e){
         e.preventDefault()
 
         try {
-            await createUserWithEmailAndPassword(auth,email,password)
+            // cadastrando usuario
+            const users = await createUserWithEmailAndPassword(auth, email, password)
+
+            // Alerta de sucesso
             alert('usuario cadastrado')
+
+            // mudando o valor da state login para true
+            setLogin(true)
+
+            // Setando valores na state detail
+            setDetail({
+                email:users.user.email,
+                id:users.user.uid
+            })
+
         } catch (error) {
-            console.log(error)
+            if(error.code === 'auth/email-already-in-use'){
+                alert('Email ja cadastrado tente outro!!')
+            } else{
+                alert('Senha fraca')
+            }
         }
     }
 
+    // function loginUser
+    async function loginUser(e){
+        e.preventDefault()
+
+        try {
+            // logando usuario
+            const users = await signInWithEmailAndPassword(auth,email,password)
+
+             // mudando o valor da state login para true
+             setLogin(true)
+
+             // Setando valores na state detail
+             setDetail({
+                 email:users.user.email,
+                 id:users.user.uid
+             })
+
+            alert('usuario logado')
+        } catch (error) {
+            if(error.code === 'auth/invalid-login-credentials'){
+                alert('Este email nao existe no app')
+            }
+        }
+    }
     return(
-        // formulario
-        <form>
-            {/* Campo de email */}
-            <div>
-                <label>Email:</label>
-                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
-            </div>
+        <div>
 
-            {/* Campo de senha */}
-            <div>
-                <label>Password:</label>
-                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
-            </div>
+            {/* formulario */}
+            <form>
 
-            {/* button */}
-            <div>
-                <button onClick={newUser}>Cadastrar</button>
-            </div>
-        </form>
+                {/* Campo de email */}
+                <div>
+                    <label>Email:</label>
+                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                </div>
+
+                {/* Campo de senha */}
+                <div>
+                    <label>Password:</label>
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                </div>
+
+                {/* button */}
+                <div>
+                    <button onClick={newUser}>Cadastrar</button>
+                    <button onClick={loginUser}>Login</button>
+                </div>
+
+            </form>
+
+            {login === true && 
+                <div>
+                    <span>{detail.email}</span>
+                    <span>{detail.id}</span>
+                </div>
+            }
+        </div>
     )
 }
